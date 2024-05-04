8da7b54a1f6e59120952b742b96e796c940c9e5f
PanGan21 <ganelis.pan@gmail.com>
2024-05-04 11:16:40 +0300

    use bin folder for cli

--- src/bin/wait-for-rs.rs
+++ src/bin/wait-for-rs.rs
@@ -1,11 +1,14 @@
 use log::LevelFilter;
-use std::{error::Error, process::exit};
+use std::process::exit;
 use structopt::StructOpt;
 
-use wait_for_rs::WaitService;
+use wait_for_rs::{Result, WaitService};
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "url-wait", about = "A command-line utility to wait for URLs.")]
+#[structopt(
+    name = "wait-for-rs",
+    about = "A command-line utility to wait for URLs."
+)]
 struct Opt {
     #[structopt(short, long)]
     urls: Vec<String>,
@@ -14,17 +17,23 @@ struct Opt {
     timeout: u64,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() {
     env_logger::builder()
         .filter_level(LevelFilter::Debug)
         .init();
 
     let opt = Opt::from_args();
 
+    if let Err(err) = run(opt) {
+        eprintln!("{}", err);
+        exit(1);
+    }
+}
+
+fn run(opt: Opt) -> Result<()> {
     let wait = WaitService::new(opt.urls, opt.timeout)?;
 
-    let result = wait.wait_for_services();
-    let exit_code: i32 = if result.is_ok() { 0 } else { 1 };
+    wait.wait_for_services()?;
 
-    exit(exit_code);
+    Ok(())
 }
