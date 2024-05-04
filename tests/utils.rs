d37648f977c509dec6f42f6dc864a90665fa9db7
PanGan21 <ganelis.pan@gmail.com>
2024-05-04 11:17:05 +0300

    add e2e tests

---
+++ tests/utils.rs
@@ -0,0 +1,63 @@
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+    ops::Drop,
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread,
+    time::Duration,
+};
+
+pub struct TestServer {
+    exiting: Arc<AtomicBool>,
+}
+
+impl TestServer {
+    pub fn new(port: u16, timeout: Duration) -> Self {
+        let exiting = Arc::new(AtomicBool::new(false));
+        let exiting_cloned = exiting.clone();
+        thread::spawn(move || {
+            let exiting = exiting_cloned;
+            thread::sleep(timeout);
+            let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).expect("can't connect");
+            listener
+                .set_nonblocking(true)
+                .expect("Cant be non-blocking");
+            for stream in listener.incoming() {
+                match stream {
+                    Ok(mut strm) => {
+                        let mut buff = Vec::with_capacity(1024);
+                        while let Ok(size) = strm.read(&mut buff) {
+                            if size == 0 {
+                                let response = b"\
+HTTP/1.1 200 OK
+Date: Sat, 01 Jan 2020 00:00:00 GMT
+Content-Type: text/html; charset=UTF-8
+Server: Test/0.0.0 (Wait-For-Rs)
+Connection: close
+
+OK
+";
+                                let _ = strm.write_all(response);
+                                break;
+                            }
+                        }
+                    }
+                    Err(_) => {}
+                }
+                if exiting.as_ref().load(Ordering::Relaxed) {
+                    break;
+                }
+            }
+        });
+        Self { exiting }
+    }
+}
+
+impl Drop for TestServer {
+    fn drop(&mut self) {
+        self.exiting.store(true, Ordering::Relaxed);
+    }
+}
---
+++ tests/wait.rs
@@ -0,0 +1,98 @@
+use std::process::Command;
+use std::time::Duration;
+
+use assert_cmd::assert::OutputAssertExt;
+use assert_cmd::cargo::CommandCargoExt;
+
+mod utils;
+
+#[test]
+fn no_args_fail() {
+    Command::cargo_bin("wait-for-rs")
+        .unwrap()
+        .assert()
+        .failure()
+        .code(1);
+}
+
+#[test]
+fn invalid_urls_fails() {
+    Command::cargo_bin("wait-for-rs")
+        .unwrap()
+        .args(&["--urls", "invalid:5000"])
+        .assert()
+        .failure()
+        .code(1);
+}
+
+#[test]
+fn multiple_addresses_one_not_working_fails() {
+    let server = utils::TestServer::new(4000, Duration::from_millis(10));
+
+    Command::cargo_bin("wait-for-rs")
+        .unwrap()
+        .args(&[
+            "--urls",
+            "127.0.0.1:4000",
+            "127.0.0.1:5000",
+            "---timeout",
+            "3",
+        ])
+        .assert()
+        .failure()
+        .code(1);
+
+    drop(server);
+}
+
+#[test]
+fn one_address_works() {
+    let server = utils::TestServer::new(4000, Duration::from_millis(10));
+
+    Command::cargo_bin("wait-for-rs")
+        .unwrap()
+        .args(&["--urls", "127.0.0.1:4000"])
+        .assert()
+        .code(0)
+        .success();
+
+    drop(server);
+}
+
+#[test]
+fn multiple_addresses_works() {
+    let servers = vec![
+        utils::TestServer::new(4000, Duration::from_millis(20)),
+        utils::TestServer::new(6000, Duration::from_millis(15)),
+    ];
+
+    Command::cargo_bin("wait-for-rs")
+        .unwrap()
+        .args(&["--urls", "127.0.0.1:4000", "127.0.0.1:6000"])
+        .assert()
+        .success()
+        .code(0);
+
+    drop(servers);
+}
+
+#[test]
+fn multiple_addresses_with_timeout_works() {
+    let server1 = utils::TestServer::new(4000, Duration::from_millis(10));
+    let server2 = utils::TestServer::new(6000, Duration::from_millis(10));
+
+    Command::cargo_bin("wait-for-rs")
+        .unwrap()
+        .args(&[
+            "--urls",
+            "127.0.0.1:4000",
+            "127.0.0.1:6000",
+            "--timeout",
+            "5",
+        ])
+        .assert()
+        .success();
+
+    drop(server1);
+    drop(server2);
+}
