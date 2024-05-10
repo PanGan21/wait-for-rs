8292b0c9ffb480d2f2a4d8324db93e92d4a6e486
PanGan21 <ganelis.pan@gmail.com>
2024-05-10 16:35:47 +0300

    add examples folder

---
+++ examples/main.rs
@@ -0,0 +1,9 @@
+use wait_for_rs::WaitService;
+
+fn main() {
+    let urls = ["google.com:443".to_string(), "github.com:443".to_string()].to_vec();
+    let timeout = 10;
+
+    let wait_service = WaitService::new(urls, timeout).unwrap();
+    wait_service.wait_for_services().unwrap();
+}
